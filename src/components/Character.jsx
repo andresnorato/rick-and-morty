35e7b3a01e6ef3ea2fb51bbedd141ebfd1c4ec45
Andres Norato <carnorat@gmail.com>
2021-07-05 10:25:15 -0500

    useReducer and list of favorites

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,18 +1,19 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "../styles/Characters.css";
 import ThemeContext from "../context/ThemeContext";
 
-const Character = ({ name, status, gender, image, species, origin }, ) => {
-  const {theme} = useContext(ThemeContext);
+const Character = ({ name, status, gender, image, species, origin, handleClick, character}) => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className={theme === 'darkmode' ? 'character darkmode': 'character'}>
+    <div className={theme === "darkmode" ? "character darkmode" : "character"}>
       <img src={image} alt={name} />
       <div className="character--info">
         <h1>{name}</h1>
-        <p>{ gender}</p>
-        <p> { species}</p>
+        <p>{gender}</p>
+        <p> {species}</p>
         <p>{origin.name}</p>
-        <p className={status === 'Alive' ? 'alive' : 'death'} >{status}</p>
+        <p className={status === "Alive" ? "alive" : "death"}>{status}</p>
+      <button type ="button" onClick={()=>handleClick(character)}>Agregar a Favoritos</button>
       </div>
     </div>
   );
